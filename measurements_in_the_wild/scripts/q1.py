37adcc4d764e02cfd95b29b14c022753acd4c1f8
Raphael Javaux <raphaeljavaux@gmail.com>
2014-04-21 16:33:17 +0200

    Q1

--- measurements_in_the_wild/scripts/q1.py
+++ measurements_in_the_wild/scripts/q1.py
@@ -2,8 +2,10 @@ ALEXA_CSV = "top-1m.csv"
 N_SITES   = 5000
 MAX_TTL   = 35
 RETRY     = 3
+TIMEOUT   = 2
 
-from itertools import islice
+from itertools import chain, islice
+from funtools  import partial
 
 def parse_top_sites(filename):
     """Parses the specified Alexa CSV file and returns the urls."""
@@ -14,11 +16,49 @@ def parse_top_sites(filename):
             yield l[:-1].split(',')[1]
 
 def traceroute(target):
-    """Sends a """
+    """
+    Sends an HTTP request to the target with a varying TTL.
+    Return the number or None if failed to reach the target.
+    
+    """
 
+    def by_tld(ans1, ans2):
+        """Compares two answers by the TLD of their respective query."""
+        query1, _ = ans1
+        query2, _ = ans2
+        return cmp(query1.tld, query2.tld)
+
+    def is_time_exceeded(ans):
+        """Returns True if the response is an time-exceeded ICMP packet."""
+        _, resp = ans
+        return isinstance(resp, ICMP)
+
+    def from_target(target_ip, ans):
+        """Returns True if the response is an time-exceeded ICMP packet."""
+        _, resp = ans
+        return isinstance(resp, ICMP)
+
+    def is_syn_ack(ans):
+        """Returns True if the response is an SYN-ACK TCP packet."""
+        _, resp = ans
+        return isinstance(resp, ICMP)
+
+    # Sends a bunch of SYN packets to reach the destination. Varies the TTL.
+    # Sends packets with TTL from 1 to MAX_TTL at the same time as it is faster.
     target_ip = socket.gethostbyname(target)
-    packet = IP(dst=target_ip, ttl=(0, MAX_TTL))/TCP(dport=80, flags="S")
+    packet    = IP(dst=target_ip, ttl=(1, MAX_TTL))/TCP(dport=80, flags="S")
+    anss, _   = sr(packet, retry=RETRY, timeout=TIMEOUT)
 
-    sr(packet, retry=RETRY)
+    if any(from_target(target_ip, ans) for ans in anss):
+        anss.sort(by_tld)
+    else:
+        # Failed to reach the target
+        return None
+
+    
+
+    n_time_exceeded = len(takewhile(is_time_exceeded, ans))
 
 sites = islice(parse_top_sites(ALEXA_CSV), stop=N_SITES)
+
+(traceroute(site) for site in sites)
