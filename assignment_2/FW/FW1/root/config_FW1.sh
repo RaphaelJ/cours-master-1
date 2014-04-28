683cc64b8816008ae59742ed69924e6646a28136
Raphael Javaux <raphaeljavaux@gmail.com>
2014-04-28 15:08:06 +0200

    règles

--- assignment_2/FW/FW1/root/config_FW1.sh
+++ assignment_2/FW/FW1/root/config_FW1.sh
@@ -5,10 +5,11 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 # Enable NAT outgoing traffic for the whole network.
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-# Permits the access to Web, PDNS, RSYNC and SMTP/IMAP from the Internet.
+# Permits the access to Web, PDNS, RSYNC (by SSH on port 2222) and SMTP/IMAP from the Internet.
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to 192.168.7.10:80
 iptables -t nat -A PREROUTING -i eth0 -p udp --dport 53 -j DNAT --to 192.168.7.11:53
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 873 -j DNAT --to 192.168.7.12:873
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 2222 -j DNAT --to 192.168.7.12:22
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 25 -j DNAT --to 192.168.2.13:25
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 143 -j DNAT --to 192.168.2.13:143
 
@@ -26,6 +27,7 @@ iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp -o eth1 -d 192.168.7.10 --dport 80 -j ACCEPT
 iptables -A FORWARD -i eth0 -p udp -o eth1 -d 192.168.7.11 --dport 53 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp -o eth1 -d 192.168.7.12 --dport 873 -j ACCEPT
+iptables -A FORWARD -i eth0 -p tcp -o eth1 -d 192.168.7.12 --dport 22 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp -o eth3 -d 192.168.2.13 --dport 25 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp -o eth3 -d 192.168.2.13 --dport 143 -j ACCEPT
 iptables -A FORWARD -i eth0 -p tcp -o eth2 -d 192.168.1.10 --dport 22 -j ACCEPT
@@ -62,7 +64,18 @@ iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.10 -o eth2 -d 192.168.1.11 --dpo
 iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.10 -o eth2 -d 192.168.1.11 --dport 21 -j ACCEPT
 
 # Lets the research department access the NFS.
-iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.10 -o eth2 -d 192.168.1.12 -j ACCEPT
-iptables -A FORWARD -i eth4 -p udp -s 192.168.3.10 -o eth2 -d 192.168.1.12 -j ACCEPT
-iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.11 -o eth2 -d 192.168.1.12 -j ACCEPT
-iptables -A FORWARD -i eth4 -p udp -s 192.168.3.11 -o eth2 -d 192.168.1.12 -j ACCEPT
+iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.10 -o eth2 -d 192.168.1.12 --dport 111 -j ACCEPT
+iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.10 -o eth2 -d 192.168.1.12 --dport 2046:2049 -j ACCEPT
+iptables -A FORWARD -i eth4 -p udp -s 192.168.3.10 -o eth2 -d 192.168.1.12 --dport 111 -j ACCEPT
+iptables -A FORWARD -i eth4 -p udp -s 192.168.3.10 -o eth2 -d 192.168.1.12 --dport 2046:2049 -j ACCEPT
+
+iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.11 -o eth2 -d 192.168.1.12 --dport 111 -j ACCEPT
+iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.11 -o eth2 -d 192.168.1.12 --dport 2046:2049 -j ACCEPT
+iptables -A FORWARD -i eth4 -p udp -s 192.168.3.11 -o eth2 -d 192.168.1.12 --dport 111 -j ACCEPT
+iptables -A FORWARD -i eth4 -p udp -s 192.168.3.11 -o eth2 -d 192.168.1.12 --dport 2046:2049 -j ACCEPT
+
+# Lets the research department access the RSYNC.
+iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.10 -o eth1 -d 192.168.7.12 --dport 873 -j ACCEPT
+iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.10 -o eth1 -d 192.168.7.12 --dport 22 -j ACCEPT
+iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.11 -o eth1 -d 192.168.7.12 --dport 873 -j ACCEPT
+iptables -A FORWARD -i eth4 -p tcp -s 192.168.3.11 -o eth1 -d 192.168.7.12 --dport 22 -j ACCEPT
